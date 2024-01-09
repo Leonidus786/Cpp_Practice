7e2d8b4cb1630f15b947826c71748553b64eaca3
Leonidus786 <abhi.upadh@gmail.com>
2024-01-09 21:31:36 +0530

    Remainder

---
+++ remainder/remainder.cpp
@@ -0,0 +1,18 @@
+// Given Two Numbers Find the remainder
+// that you will get on dividing the first number by second number.
+
+// Psuedocode for this-->
+// start 
+// Read a and b
+// End
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+
+	cout<<"Enter two nos";
+	cin>>a>>b;
+
+	cout<<"The output is: "<<(a%b);
+}
