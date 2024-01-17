0324baf893937bb8eb9d0613fca2d743dddc0a47
Leonidus786 <abhi.upadh@gmail.com>
2024-01-18 00:03:22 +0530

    loops-1

---
+++ loops/for.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    // cin>>n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"i="<<i<<endl;
+    }
+
+
+    cout<<"\n\n For loop using pre increment\n";
+
+	for (int i = 0; i < n; ++i)
+	{
+		cout<<"i="<<i<<endl;
+	}
+
+
+
+
+    int i=5;
+		int k=2;
+		while(i<10)
+		{
+			cout<<(k++);
+			i++;
+		}
+
+
+		int k=2;
+		for(int i=5;i<10;i++)
+		{
+			cout<<(k++);
+		}
+
+
+    
+}
